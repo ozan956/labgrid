99a5db5dfe9263de8a0cd93f168d3c4d4b962a11
Ozan Durgut <ozan.durgut@analog.com>
2026-07-13 12:45:12 +0200

    resource: add FTDI GPIO resources
    Add local and remote resources for FTDI data-bus GPIO lines exposed
    through asynchronous bit-bang mode.
    
    The resource models one input- or output-capable bit on a selected FTDI
    interface and exports the USB identity, interface, index, inversion,
    and direction metadata needed by the driver.
    
    Signed-off-by: Ozan Durgut <ozan.durgut@analog.com>

--- labgrid/resource/udev.py
+++ labgrid/resource/udev.py
@@ -799,6 +799,48 @@ class HIDRelay(USBResource):
 
 @target_factory.reg_resource
 @attr.s(eq=False)
+class FTDIGPIO(USBResource):
+    """This resource describes a single GPIO line on an FTDI bit-bang interface."""
+
+    index = attr.ib(
+        default=0,
+        converter=int,
+        validator=attr.validators.and_(
+            attr.validators.instance_of(int),
+            attr.validators.ge(0),
+            attr.validators.le(7),
+        ),
+    )
+    interface = attr.ib(
+        default=1,
+        converter=int,
+        validator=attr.validators.and_(
+            attr.validators.instance_of(int),
+            attr.validators.ge(1),
+        ),
+    )
+    invert = attr.ib(default=False, validator=attr.validators.instance_of(bool))
+    direction = attr.ib(default="out", validator=attr.validators.in_(("in", "out")))
+
+    def __attrs_post_init__(self):
+        self.match["DEVTYPE"] = "usb_device"
+        super().__attrs_post_init__()
+
+    def filter_match(self, device):
+        match = (
+            device.properties.get("ID_VENDOR_ID"),
+            device.properties.get("ID_MODEL_ID"),
+        )
+        if match not in [("0403", "6010"),  # FT2232C/D/H Dual UART/FIFO IC
+                         ("0403", "6011"),  # FT4232H Quad UART/MPSSE IC
+                         ("0403", "6014"),  # FT232HL/Q
+                         ]:
+            return False
+
+        return super().filter_match(device)
+
+@target_factory.reg_resource
+@attr.s(eq=False)
 class USBFlashableDevice(USBResource):
     @property
     def devnode(self):
